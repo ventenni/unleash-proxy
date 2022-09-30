e9f20f7f10cb620e4192dc816bb292d22c5eb194
Nick Vaiente <nick.vaiente@ttt.studio>
2022-09-30 14:36:37 -0700

    updated API key

--- proxy.js
+++ proxy.js
@@ -11,7 +11,8 @@ const EnableAfter = require('./CustomStrategies/EnableAfter');
 const app = createApp({
 	unleashUrl: 'http://unleash-lb-391482345.us-east-2.elb.amazonaws.com/api',
 	unleashApiToken:
-		'*:development.16d79bb7fea5b528138e682a688d996996fc443ff72b0b2c6e3b763e',
+		// '*:development.16d79bb7fea5b528138e682a688d996996fc443ff72b0b2c6e3b763e',
+		'*:*.136d33e6bd0a83870227470d5e5e39eba3d30c41829efb3c2bd6debc',
 	clientKeys: ['proxy-1'],
 	proxyPort: 3001,
 	customStrategies: [
