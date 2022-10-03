892bca09741e959c5eccac617a06e14e388bad42
Nick Vaiente <nick.vaiente@ttt.studio>
2022-10-03 11:41:43 -0700

    replaced api key

--- proxy.js
+++ proxy.js
@@ -11,7 +11,8 @@ const app = createApp({
 	unleashUrl: 'http://unleash-lb-391482345.us-east-2.elb.amazonaws.com/api',
 	unleashApiToken:
 		// '*:development.16d79bb7fea5b528138e682a688d996996fc443ff72b0b2c6e3b763e',
-		process.env.AWS_INSTANCE_API_KEY,
+		// process.env.AWS_INSTANCE_API_KEY,
+		'*:development.e9d1bc856ba87529568cdcb5b8d5c2457f23beddc7cea01804e0c531',
 	clientKeys: ['proxy-1'],
 	proxyPort: 3001,
 	customStrategies: [
