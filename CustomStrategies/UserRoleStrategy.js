bd7e9de6b035f51ec639a77c5ab4804e1bbf8b92
Nick Vaiente <nick.vaiente@ttt.studio>
2022-10-12 11:10:27 -0700

    added user role strategy

---
+++ CustomStrategies/UserRoleStrategy.js
@@ -0,0 +1,24 @@
+const { Strategy } = require('unleash-client');
+
+class UserRoleStrategy extends Strategy {
+	constructor() {
+		super('UserRole');
+	}
+
+	isEnabled(parameters, context) {
+		console.log({ parameters, context });
+		let isUserRole = false;
+
+		const roles = parameters.UserRole.split(',');
+
+		[...roles].forEach((item) => {
+			if (item.toLowerCase() === context.properties.UserRole.toLowerCase()) {
+				isUserRole = true;
+			}
+		});
+
+		return isUserRole;
+	}
+}
+
+module.exports = UserRoleStrategy;
--- proxy.js
+++ proxy.js
@@ -1,24 +1,25 @@
 const port = 3001;
 
-require("dotenv").config();
-const { createApp } = require("@unleash/proxy");
+require('dotenv').config();
+const { createApp } = require('@unleash/proxy');
 
-const IsInAmerica = require("./CustomStrategies/IsInAmerica");
-const DateTimeIsBetweenRange = require("./CustomStrategies/DateTimeIsBetweenRange");
-const TargetByEmailProviderStrategy = require("./CustomStrategies/TargetUserEmailProvider");
-const TargetByOSStrategy = require("./CustomStrategies/TargetByOS");
-const EnableAfter = require("./CustomStrategies/EnableAfter");
+const IsInAmerica = require('./CustomStrategies/IsInAmerica');
+const DateTimeIsBetweenRange = require('./CustomStrategies/DateTimeIsBetweenRange');
+const TargetByEmailProviderStrategy = require('./CustomStrategies/TargetUserEmailProvider');
+const TargetByOSStrategy = require('./CustomStrategies/TargetByOS');
+const EnableAfter = require('./CustomStrategies/EnableAfter');
+const UserRoleStrategy = require('./CustomStrategies/UserRoleStrategy');
 
 const app = createApp({
-	unleashUrl: "http://unleash-lb-391482345.us-east-2.elb.amazonaws.com/api",
+	unleashUrl: 'http://unleash-lb-391482345.us-east-2.elb.amazonaws.com/api',
 	// unleashUrl: 'http://localhost:4242/api',
 	unleashApiToken:
 		// LOCAL ENV API KEY
 		// '*:development.16d79bb7fea5b528138e682a688d996996fc443ff72b0b2c6e3b763e',
 
 		// AWS ENV API KEY
-		"*:development.e9d1bc856ba87529568cdcb5b8d5c2457f23beddc7cea01804e0c531",
-	clientKeys: ["proxy-1"],
+		'*:development.e9d1bc856ba87529568cdcb5b8d5c2457f23beddc7cea01804e0c531',
+	clientKeys: ['proxy-1'],
 	proxyPort: 3001,
 	customStrategies: [
 		new DateTimeIsBetweenRange(),
@@ -26,6 +27,7 @@ const app = createApp({
 		new IsInAmerica(),
 		new TargetByOSStrategy(),
 		new TargetByEmailProviderStrategy(),
+		new UserRoleStrategy(),
 	],
 });
 
